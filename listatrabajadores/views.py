1e1dee3cdb64a81db0c94c516459e52a3da72b2e
123apptest <iamyoudad993812690@gmail.com>
2022-11-25 03:25:57 -0300

    Parte de Diseña

---
+++ listatrabajadores/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+def login(request):
+    
+    return render(request, "login.html")
+
+def index(request):
+    
+    return render(request, "index.html")
+
+def trabajador(request):
+    
+    return render(request, "trabajador.html")
+
+
+
+    
